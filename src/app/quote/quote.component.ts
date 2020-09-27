7230304ebef475ecf80d1c4877929862971a35a3
Fred <akamaatei@gmail.com>
2020-09-27 15:52:58 +0300

    add date details

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'Love', 'Spread love everywhere you go'),
-    new Quote(2, 'Growth', 'When you reach the end of your rope, tie a knot in it and hang on'),
-    new Quote(3, 'Happiness', 'Whoever is happy will make others happy too'),
-    new Quote(4, 'Life', 'In the end, it is not the years in your life that count. It is the life in your years'),
-    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Quote(2, 'Buy Cookies', 'I have to buy cookies for the parrot'),
+    new Quote(1, 'Love', 'Spread love everywhere you go', new Date()),
+    new Quote(2, 'Growth', 'When you reach the end of your rope, tie a knot in it and hang on', new Date()),
+    new Quote(3, 'Happiness', 'Whoever is happy will make others happy too', new Date()),
+    new Quote(4, 'Life', 'In the end, it is not the years in your life that count. It is the life in your years', new Date()),
+    new Quote(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date()),
+    new Quote(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date()),
   ]
 
   toggleDetails(index) {

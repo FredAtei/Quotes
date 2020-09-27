5e91dac8ba7d8f81aac3ef93214c1b0ecf69837c
Fred <akamaatei@gmail.com>
2020-09-27 11:59:32 +0300

    add test subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes: string[];
-
-  constructor() {
-    this.quotes = ['be happy, be grateful, be joyful']
-  }
+  quotes: Quote[] = [
+    { id: 1, name: 'Spread love everywhere you go' },
+    { id: 2, name: 'Spread love everywhere you go' },
+    { id: 3, name: 'Spread love everywhere you go' },
+    { id: 4, name: 'Spread love everywhere you go' },
+    { id: 5, name: 'Spread love everywhere you go' },
+    { id: 6, name: 'Spread love everywhere you go' },
+  ]
 }

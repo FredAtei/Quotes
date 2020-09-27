79346fd35edb7059866f664b569563bff1d9093f
Fred <akamaatei@gmail.com>
2020-09-27 13:56:53 +0300

    add toggle effect

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,15 +9,19 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    { id: 1, name: 'Love', description: 'Spread love everywhere you go' },
-    { id: 2, name: 'Growth', description: 'When you reach the end of your rope, tie a knot in it and hang on.' },
-    { id: 3, name: 'Happiness', description: 'Whoever is happy will make others happy too.' },
-    { id: 4, name: 'Life', description: 'In the end, it is not the years in your life that count. It is the life in your years.' },
+    new Quote(1, 'Love', 'Spread love everywhere you go'),
+    new Quote(2, 'Growth', 'When you reach the end of your rope, tie a knot in it and hang on'),
+    new Quote(3, 'Happiness', 'Whoever is happy will make others happy too'),
+    new Quote(4, 'Life', 'In the end, it is not the years in your life that count. It is the life in your years'),
   ]
+
+  toggleDetails(index) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }

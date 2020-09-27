642ec64186b92d1cb744ed77c983b1029c5ae00b
Fred <akamaatei@gmail.com>
2020-09-27 12:39:07 +0300

    modify component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,10 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes: Quote[] = [
-    { id: 1, name: 'Spread love everywhere you go' },
-    { id: 2, name: 'Spread love everywhere you go' },
-    { id: 3, name: 'Spread love everywhere you go' },
-    { id: 4, name: 'Spread love everywhere you go' },
-    { id: 5, name: 'Spread love everywhere you go' },
-    { id: 6, name: 'Spread love everywhere you go' },
+    { id: 1, name: 'Love', description: 'Spread love everywhere you go' },
+    { id: 2, name: 'Growth', description: 'When you reach the end of your rope, tie a knot in it and hang on.' },
+    { id: 3, name: 'Happiness', description: 'Whoever is happy will make others happy too.' },
+    { id: 4, name: 'Life', description: 'In the end, it is not the years in your life that count. It is the life in your years.' },
+
   ]
 }

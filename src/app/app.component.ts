fc0c7ad11fea0b37e3c103fa64c5673c64668f5f
Fred <akamaatei@gmail.com>
2020-09-27 13:25:53 +0300

    add new description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,9 @@ import { Quote } from './quote';
 })
 export class AppComponent {
   quotes: Quote[] = [
-    { id: 1, name: 'Love', description: 'Spread love everywhere you go' },
-    { id: 2, name: 'Growth', description: 'When you reach the end of your rope, tie a knot in it and hang on.' },
-    { id: 3, name: 'Happiness', description: 'Whoever is happy will make others happy too.' },
-    { id: 4, name: 'Life', description: 'In the end, it is not the years in your life that count. It is the life in your years.' },
-
+    // { id: 1, name: 'Love' },
+    // { id: 2, name: 'Growth' },
+    // { id: 3, name: 'Happiness' },
+    // { id: 4, name: 'Life', },
   ]
 }

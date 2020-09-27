c6f8181640bfa9b123be731f53335395ce7a5049
Fred <akamaatei@gmail.com>
2020-09-27 16:15:11 +0300

    modify file

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -14,6 +14,10 @@ export class QuoteDetailComponent implements OnInit {
   quoteComplete(complete: boolean) {
     this.isComplete.emit(complete);
   }
+
+  quoteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
